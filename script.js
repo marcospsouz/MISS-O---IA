c0b9b7ea5d4de6b07a25d6301d475d8cbb748245
marcos/psouza <137806232+marcospsouz@users.noreply.github.com>
2025-05-20 07:52:24 -0300

    fds

--- script.js
+++ script.js
@@ -26,3 +26,10 @@ const perguntas = [
     alternativas: ["Apenas Golfinho", "Golfinho e Morcego"],
   },
 ];
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPergunta();
