2b56f8e9dd6ef52b5bedccfba3c8d5b6309c46bb
marcos/psouza <137806232+marcospsouz@users.noreply.github.com>
2025-05-27 08:35:43 -0300

    fdz

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ function mostraPergunta() {
 function mostraAlternativas() {
   for (const alternativa of perguntaAtual.alternativas) {
       const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
   }
 }
 mostraPergunta();
