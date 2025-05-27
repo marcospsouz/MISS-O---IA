89a110bba954644fd9ed8cfcc629e9f0909dc9db
marcos/psouza <137806232+marcospsouz@users.noreply.github.com>
2025-05-27 11:20:06 -0300

    fds

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ const perguntas = [
 ];
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 function mostraPergunta() {
   perguntaAtual = perguntas[atual];
   caixaPerguntas.textContent = perguntaAtual.enunciado;
