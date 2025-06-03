8be754f485d7b35fa90176465801582bfe94d709
marcos/psouza <137806232+marcospsouz@users.noreply.github.com>
2025-06-03 11:17:18 -0300

    ds

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-  [
     {
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
@@ -71,7 +70,6 @@ const perguntas = [
             }
         ]
     },
-]
 ];
 let atual = 0;
 let perguntaAtual;
